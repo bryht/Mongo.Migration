27b1cedee7781409caf5e052c50b9874747d2941
Ming Li <ming.li@yunify.nl>
2019-04-09 13:27:39 +0200

    remove .net 47

--- Mongo.Migration/Services/Migration/OnStartup/MigrationOnStartup.cs
+++ Mongo.Migration/Services/Migration/OnStartup/MigrationOnStartup.cs
@@ -1,7 +1,11 @@
+using Mongo.Migration.Documents.Locators;
+using Mongo.Migration.Migrations.Locators;
+
 namespace Mongo.Migration.Services.Migration.OnStartup
 {
     public class MigrationOnStartup : IMigrationStrategy
     {
+
         public void Migrate()
         {
             throw new System.NotImplementedException();
